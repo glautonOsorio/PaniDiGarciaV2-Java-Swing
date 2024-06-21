4d0925482c1f043302d9ebc1eac33a072d2fd681
Glauton <glauton.osorio@alunos.sc.senac.br>
2024-06-21 16:20:15 -0300

    feat: estilização de login feita

--- PaniDiGarciaV2/src/view/FrameLogin.java
+++ PaniDiGarciaV2/src/view/FrameLogin.java
@@ -53,32 +53,37 @@ public class FrameLogin {
 		frame.getContentPane().setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Login Frame!");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setForeground(new Color(0, 0, 0));
 		lblNewLabel.setBackground(new Color(64, 0, 64));
 		lblNewLabel.setFont(new Font("Serif", Font.PLAIN, 38));
-		lblNewLabel.setBounds(267, 61, 270, 72);
+		lblNewLabel.setBounds(223, 61, 314, 72);
 		frame.getContentPane().add(lblNewLabel);
 		
 		JLabel lblEmail = new JLabel("Email:");
+		lblEmail.setHorizontalAlignment(SwingConstants.LEFT);
 		lblEmail.setForeground(new Color(0, 0, 0));
 		lblEmail.setFont(new Font("Serif", Font.PLAIN, 28));
 		lblEmail.setBackground(new Color(64, 0, 64));
-		lblEmail.setBounds(223, 129, 205, 72);
+		lblEmail.setBounds(223, 144, 300, 57);
 		frame.getContentPane().add(lblEmail);
 		
 		tfEmail = new JTextField();
+		tfEmail.setHorizontalAlignment(SwingConstants.CENTER);
 		tfEmail.setBounds(223, 194, 300, 40);
 		frame.getContentPane().add(tfEmail);
 		tfEmail.setColumns(10);
 		
 		JLabel lblPassword = new JLabel("Password:");
+		lblPassword.setHorizontalAlignment(SwingConstants.LEFT);
 		lblPassword.setForeground(Color.BLACK);
 		lblPassword.setFont(new Font("Serif", Font.PLAIN, 28));
 		lblPassword.setBackground(new Color(64, 0, 64));
-		lblPassword.setBounds(223, 220, 205, 72);
+		lblPassword.setBounds(223, 235, 300, 57);
 		frame.getContentPane().add(lblPassword);
 		
 		passwordField = new JPasswordField();
+		passwordField.setHorizontalAlignment(SwingConstants.CENTER);
 		passwordField.setBounds(225, 287, 300, 40);
 		frame.getContentPane().add(passwordField);
 		
